f775e44d6c565216cd938bf6db0480a199fe5091
Gael BERTIN <gbertin@e1r1p8.clusters.42paris.fr>
2022-11-02 08:44:57 +0100

    fix pull

--- cpp01/ex06/Harl.cpp
+++ cpp01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: gbertin <gbertin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/28 17:38:49 by gbertin           #+#    #+#             */
-/*   Updated: 2022/10/30 14:18:33 by gbertin          ###   ########.fr       */
+/*   Updated: 2022/10/31 14:57:28 by gbertin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,22 +30,30 @@ void	Harl::complain(std::string level)
 	for (int i = 0; i < 4; i++)
 	{
 		if (level == gravity[i])
-			(this->*f[i])();
+		{
+			for (int y = i; y < 4; y++)
+				(this->*f[y])();
+			i = 4;
+		}
 	}
 }
 
 void	Harl::debug(void) {
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do !" << std::endl;
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special ketchup burger. I really do !" << std::endl << std::endl;
 }
 
 void	Harl::info(void) {
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger ! If you did, I wouldn't be asking for more !" << std::endl;
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger ! If you did, I wouldn't be asking for more !" << std::endl << std::endl;
 }
 
 void	Harl::warning(void) {
-	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl << std::endl;
 }
 
 void	Harl::error(void) {
-	std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl;
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable ! I want to speak to the manager now." << std::endl << std::endl;
 }
--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,20 +6,46 @@
 /*   By: gbertin <gbertin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/29 08:30:27 by gbertin           #+#    #+#             */
-/*   Updated: 2022/10/30 14:12:47 by gbertin          ###   ########.fr       */
+/*   Updated: 2022/10/31 14:59:21 by gbertin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+#include <iostream>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	Harl harl;
-
-	harl.complain("DEBUG");
-	harl.complain("INFO");
-	harl.complain("WARNING");
-	harl.complain("ERROR");
 	
+	if (argc == 2)
+	{
+		int			i = 0;
+		std::string levelMin = argv[1];
+		std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+		
+		while (i < 4)
+		{
+			if (levelMin == levels[i])
+				break ;
+			i++;
+		}
+		switch (i) {
+			case 0:
+				harl.complain("DEBUG");
+				break ;
+			case 1:
+				harl.complain("INFO");
+				break ;
+			case 2:
+				harl.complain("WARNING");
+				break ;
+			case 3:
+				harl.complain("ERROR");
+				break ;
+			default:
+				std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+				break ;
+		}
+	}
 	return (0);
 }
